d929bfbb0ec9786033a739de73f23ff25f3d5d72
hjw <2232285004@qq.com>
2023-08-10 17:59:51 +0800

    Merge branch 'master' of https://gitee.com/ququbudu/takeaway

---
+++ src/main/java/com/hjw/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.hjw.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.ResolvableType;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisConnectionUtils;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig
+{
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory)
+    {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+
+        // 默认的 key 序列化器为 JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        return redisTemplate;
+
+    }
+
+}
