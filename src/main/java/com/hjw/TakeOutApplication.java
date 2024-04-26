cf0dc1018f69e1ea31a43fab69dd263fd6076d19
何嘉炜 <hejiawei3@xiaomi.com>
2024-04-26 10:33:52 +0800

    修改代码格式

--- src/main/java/com/hjw/TakeOutApplication.java
+++ src/main/java/com/hjw/TakeOutApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
 @Slf4j
+@SpringBootApplication
 @ServletComponentScan
 @EnableTransactionManagement
 @EnableCaching
@@ -16,7 +16,6 @@ public class TakeOutApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TakeOutApplication.class, args);
-//        System.out.println();
     }
 
 }
